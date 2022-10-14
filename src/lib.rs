0d665ee8712ffdc84926adcf815a7817b81b7363
Scott Williams <scott@mightydevco.com>
2022-10-14 16:33:20 -0500

    updates

--- src/lib.rs
+++ src/lib.rs
@@ -1,79 +1,117 @@
-use wasm_membrane_guest::membrane::log;
-use mechtron::{Mechtron, mechtron_register, MechtronCtx, MechtronFactory};
-use mechtron::error::Error;
-use mesh_portal::version::latest::entity::request::Action;
-use mesh_portal::version::latest::entity::response::ResponseCore;
-use mesh_portal::version::latest::http::HttpRequest;
-use mesh_portal::version::latest::msg::MsgRequest;
-use mesh_portal::version::latest::resource::ResourceStub;
+#![allow(warnings)]
 
+#[macro_use]
+extern crate lazy_static;
 
-/// mechtron_init() is called after the Wasm file has been compiled by Starlane.
-/// It's most important job is to register any mechtron factories.
+#[macro_use]
+extern crate cosmic_macros;
+
+use cosmic_macros::handler_sync;
+use cosmic_space::err::SpaceErr;
+use cosmic_space::log::{PointLogger, RootLogger};
+use cosmic_space::particle::Details;
+use cosmic_space::wave::core::CoreBounce;
+use cosmic_space::substance::Substance;
+use cosmic_space::wave::exchange::synch::{
+    DirectedHandler, InCtx, ProtoTransmitter, ProtoTransmitterBuilder, RootInCtx,
+};
+use mechtron::err::{GuestErr, MechErr};
+use mechtron::guest::GuestSkel;
+use mechtron::{guest, Guest, MechtronFactories, MechtronFactory, Platform};
+use mechtron::{Mechtron, MechtronLifecycle, MechtronSkel};
+use std::marker::PhantomData;
+use std::sync::Arc;
+
 #[no_mangle]
-pub extern "C" fn mechtron_init()
-{
-    /// Here we register the MyMechtronFactory for creating '{{project-name}}' Mechtrons
-    mechtron_register(Box::new(MyMechtronFactory::new()));
+pub extern "C" fn mechtron_guest(details: Details) -> Result<Arc<dyn mechtron::Guest>, GuestErr> {
+    Ok(Arc::new(mechtron::guest::Guest::new(
+        details,
+        MyPlatform::new(),
+    )?))
 }
 
-/// Factory implementation for MyMechtron Mechtron
-pub struct MyMechtronFactory { }
+#[derive(Clone)]
+pub struct MyPlatform;
+
+impl Platform for MyPlatform {
+    type Err = GuestErr;
+    fn factories(&self) -> Result<MechtronFactories<Self>, Self::Err>
+    where
+        Self: Sized,
+    {
+        let mut factories = MechtronFactories::new();
+        factories.add(MyMechtronFactory::new());
+        Ok(factories)
+    }
+}
+
+impl MyPlatform {
+    pub fn new() -> Self {
+        Self {}
+    }
+}
+
+pub struct MyMechtronFactory {}
 
 impl MyMechtronFactory {
     pub fn new() -> Self {
-        Self{}
+        Self {}
     }
 }
 
-impl MechtronFactory for MyMechtronFactory {
-
-    /// Here we returning the very important Mechtron name which will must be referenced in the Mechtron config
-    fn mechtron_name(&self) -> String {
-        "{{project-name}}".to_string()
+impl<P> MechtronFactory<P> for MyMechtronFactory
+where
+    P: Platform + 'static,
+{
+    fn name(&self) -> String {
+        "my-mechtron".to_string()
     }
 
-    fn create(&self, stub: ResourceStub) -> Result<Box<dyn Mechtron>, Error> {
-        Ok(Box::new(MyMechtron::new()))
-    }
-}
-
-
-/// MyMechtron mechtron is an implementation of the Mechtron trait.
-/// It handles requests and produces Responses
-pub struct MyMechtron {}
-
-impl MyMechtron {
-    pub fn new()->Self{
-        Self{}
-    }
-}
-
-impl Mechtron for MyMechtron  {
-
-    /// Write custom Msg request handler code here
-    fn handle_msg_request(
-        &self,
-        ctx: &dyn MechtronCtx,
-        request: MsgRequest,
-    ) -> Result<ResponseCore, Error> {
-        Ok(request.fail(format!(
-            "Mechtron '{}' does not have a Msg handler implementation",
-            ctx.stub().address.to_string()
-        ).as_str()))
+    fn lifecycle(&self, skel: MechtronSkel<P>) -> Result<Box<dyn MechtronLifecycle<P>>, P::Err> {
+        Ok(Box::new(MyMechtron::restore(skel, (), ())))
     }
 
-    /// Write custom Http request handler code here
-    fn handle_http_request(
-        &self,
-        ctx: &dyn MechtronCtx,
-        request: HttpRequest,
-    ) -> Result<ResponseCore, Error> {
-        Ok(request.fail(format!(
-            "Mechtron '{}' does not have an Http handler implementation",
-            ctx.stub().address.to_string()
-        ).as_str()))
+    fn handler(&self, skel: MechtronSkel<P>) -> Result<Box<dyn DirectedHandler>, P::Err> {
+        Ok(Box::new(MyMechtron::restore(skel, (), ())))
     }
 
 }
 
+pub struct MyMechtron<P>
+where
+    P: Platform + 'static,
+{
+    skel: MechtronSkel<P>,
+}
+
+impl<P> Mechtron<P> for MyMechtron<P>
+where
+    P: Platform + 'static,
+{
+    type Skel = MechtronSkel<P>;
+    type Cache = ();
+    type State = ();
+
+    fn restore(skel: Self::Skel, _cache: Self::Cache, _state: Self::State) -> Self {
+        MyMechtron { skel }
+    }
+}
+
+impl<P> MechtronLifecycle<P> for MyMechtron<P> where P: Platform + 'static {}
+
+#[handler_sync]
+impl<P> MyMechtron<P>
+where
+    P: Platform + 'static,
+{
+    #[route("Ext<Hello>")]
+    pub fn hello(&self, _: InCtx<'_, ()>) -> Result<Substance, P::Err> {
+        Ok(Substance::Text("Goodbye".to_string()))
+    }
+}
+
+#[cfg(test)]
+pub mod test {
+    #[test]
+    pub fn test() {}
+}
